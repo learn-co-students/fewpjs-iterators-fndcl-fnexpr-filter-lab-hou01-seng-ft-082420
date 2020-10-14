2bab84ac203ae8f9ae6401e96ae09f7ac0bda825
IainLR <iainrobertzon@gmail.com>
2020-10-14 15:25:20 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+const findMatching = (array, name) => {
+    let drivers = array.filter(driver => driver.toLowerCase() == name.toLowerCase())
+    return drivers
+}
+
+const fuzzyMatch = (array, name) => {
+    let letters = name.split('')[0] + name.split('')[1]
+    let drivers = array.filter(driver => driver.substring(0, 2) == letters )
+    return drivers
+}
+
+const matchName = (array, name) => {
+    let drivers = array.filter(driver => driver.name == name)
+    return drivers
+}
